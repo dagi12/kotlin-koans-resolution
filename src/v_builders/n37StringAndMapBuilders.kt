1241edabfc2d6e4538da31b8fbec8715a2c1227d
dagi12 <dagi12@o2.pl>
2017-11-19 10:22:52 +0100

    builders

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,9 @@
 package v_builders
 
 import util.TODO
+import v_builders.examples.buildMap
 import java.util.*
+import kotlin.collections.HashMap
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -20,7 +22,7 @@ fun buildStringExample(): String {
 }
 
 fun todoTask37(): Nothing = TODO(
-    """
+        """
         Task 37.
         Uncomment the commented code and make it compile.
         Add and implement function 'buildMap' with one parameter (of type extension function) creating a new HashMap,
@@ -29,11 +31,17 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+
+    fun buildMap(buildMap: HashMap<Int, String>.() -> Unit): Map<Int, String> {
+        val map = HashMap<Int, String>()
+        map.buildMap()
+        return map
+    }
+
+    return buildMap {
+        this.put(0, "0")
+        for (i in 1..10) {
+            this.put(i, "$i")
+        }
+    }
 }
